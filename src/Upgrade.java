ac91e518d2aca7f51f869c6b70c2ac97a4b61e97
marlon-schmitz-289 <marlonschmitz289@gmail.com>
2021-03-21 15:29:01 +0100

    Json Properties

--- src/Upgrade.java
+++ src/Upgrade.java
@@ -1,9 +1,16 @@
 
 @SuppressWarnings("unused")
 public class Upgrade {
+	@JsonProperty("Name")
 	private String name;
+	
+	@JsonProperty("Kosten")
 	private double cost;
+	
+	@JsonProperty("Level")
 	private int level = 1;
+	
+	@JsonProperty("MaxLevel")
 	private int maxLevel = 50;
 	
 	
